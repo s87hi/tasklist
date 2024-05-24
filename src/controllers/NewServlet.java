f818665557a7214c9c8ec8fb11308fb5e2c191a3
shino.hanasaki <hanasaki.s@wingarc.com>
2024-05-24 13:55:42 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,9 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,25 +35,14 @@ public class NewServlet extends HttpServlet {
         EntityManager em = DBUtil.createEntityManager();
         em.getTransaction().begin();
 
-        //Taskのインスタンスを生成
-        Task t = new Task();
+     // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        //tの各フィールドにデータを代入
-        String content ="ZOZOを見る";
-        t.setContent(content);
+        // おまじないとしてのインスタンスを生成
+        request.setAttribute("task", new Task());
 
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
-
-        t.setCreated_at(currentTime);
-        t.setUpdated_at(currentTime);
-
-        // データベースに保存
-        em.persist(t);
-        em.getTransaction().commit();
-
-        // 自動採番されたIDの値を表示
-        response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
     }
 
 }
